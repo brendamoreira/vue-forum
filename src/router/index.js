23b645fee80ad3f055d4d17a254eea1cb94d693e
Brenda Moreira <brendanatalia94@hotmail.com>
2022-05-09 12:35:52 +0200

    add forum page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import HomePage from "@/pages/HomePage";
 import ThreadShow from "@/pages/ThreadShow";
+import ForumPage from "@/pages/ForumPage"
 import NotFound from "@/pages/NotFound";
 import { createRouter, createWebHistory } from "vue-router";
 import sourceData from "@/data.json"
 
+
 const routes = [
   { path: "/", name: "Home", component: HomePage },
+  { path: "/forum/:id", name: "Forum", component: ForumPage, props: true},
   { path: "/thread/:id", name: "ThreadShow", component: ThreadShow, props: true , beforeEnter(to, from, next) {
     //check if thread exists
     const threadExists = sourceData.threads.find(thread=> thread.id === to.params.id)
