4dfe1c5b3824b61be2b49914bc5bc54aef00a9b9
Brenda Moreira <brendanatalia94@hotmail.com>
2022-06-03 16:50:08 +0200

    add redirect on submit and cancel thread create

--- src/store/index.js
+++ src/store/index.js
@@ -41,7 +41,7 @@ export default createStore({
     updateUser({ commit }, user) {
       commit("setUser", { user, userId: user.id });
     },
-    createThread({ commit, state, dispatch }, { text, title, forumId }) {
+    async createThread({ commit, state, dispatch }, { text, title, forumId }) {
       const id = "gggg" + Math.random();
       const userId = state.authId;
       const publishedAt = Math.floor(Date.now() / 1000);
@@ -50,6 +50,7 @@ export default createStore({
       commit('appendThreadToUser', {userId, threadId: id})
       commit('appendThreadToForum', {forumId, threadId: id})
       dispatch('createPost', { text, threadId: id})
+      return state.threads.find(thread => thread.id === id)
     },
   },
   mutations: {
